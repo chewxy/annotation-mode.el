b4c24440bade080a8c15fc7cbd140f5cbb13537d
chewxy <chewxy@gmail.com>
2021-11-18 10:43:38 +1100

    Updated the space-pad program to also accept directories

--- cmd/space-pad/main.go
+++ cmd/space-pad/main.go
@@ -4,7 +4,10 @@ import (
 	"bufio"
 	"log"
 	"os"
+	"path/filepath"
 	"strings"
+
+	"github.com/pkg/errors"
 )
 
 func pad(lines []string, max int) []string {
@@ -19,8 +22,7 @@ func pad(lines []string, max int) []string {
 	return lines
 }
 
-func main() {
-	filename := os.Args[1]
+func padOneFile(filename string) error {
 	f, err := os.OpenFile(filename, os.O_RDWR, 0644)
 	if err != nil {
 		log.Fatal(err)
@@ -32,7 +34,7 @@ func main() {
 		lines = append(lines, scanner.Text())
 	}
 	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
+		return errors.Wrapf(err, "Failed to scan %v", f.Name())
 	}
 
 	var max int
@@ -47,12 +49,45 @@ func main() {
 
 	// write
 	if _, err := f.Seek(0, 0); err != nil {
-		log.Fatal(f)
+		return errors.Wrapf(err, "Seek(0,0) failed on %v", f.Name())
 	}
+
 	for _, l := range lines {
 		f.WriteString(l)
 		f.WriteString("\n")
 	}
 	f.Close()
+	return nil
+}
+
+func main() {
+	arg1 := os.Args[1]
+	info, err := os.Stat(arg1)
+	if os.IsNotExist(err) {
+		log.Fatalf("%v does not exist", os.Args[1])
+	}
+	if info.IsDir() {
+		matches, err := filepath.Glob(filepath.Join(arg1, "*.txt"))
+		if err != nil {
+			log.Fatal(err)
+		}
+		var errs []error
+		for _, filename := range matches {
+			if err := padOneFile(filename); err != nil {
+				errs = append(errs, err)
+			}
+		}
+		if len(errs) != 0 {
+			for _, err := range errs {
+				log.Println(err.Error())
+			}
+			os.Exit(1)
+		}
+	} else {
+		filename := arg1
+		if err := padOneFile(filename); err != nil {
+			log.Fatal(err)
+		}
+	}
 
 }
